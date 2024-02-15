4e1b1fa2bf766d86e53a00fc4953f47122b91a2b
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-02-15 14:49:06 +0500

    Menu

---
+++ Student Management System/menu.php
@@ -0,0 +1,10 @@
+
+        
+        </head>
+<div class=nav>
+	<a class=nav_text href="index.php">Home</a> 
+	<a class=nav_text href="addstudent.php">Sign Up</a> 
+	<a class=nav_text href="studentlogin.php">Login</a> 
+	 
+    
+  </div>
