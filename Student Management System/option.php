bc6d1d87b9fe60faeb27cb2008ce0696c5cfc401
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-02-16 22:02:26 +0500

    Options

---
+++ Student Management System/option.php
@@ -0,0 +1,7 @@
+<ul align="right">
+
+    <li><a href="studentarea.php">Home</a></li>
+    <li><a href="editprofile.php">Edit Profile</a></li>
+    <li><a href="studentlogout.php">Logout</a></li>
+
+</ul>
